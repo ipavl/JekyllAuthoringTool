ec96ae8af034a16456752c63ea73d34916808e97
ian <ian@arch>
2014-06-22 01:22:02 -0400

    Finishing up file saving

--- jat/app/controllers/jat_controller.rb
+++ jat/app/controllers/jat_controller.rb
@@ -1,22 +1,30 @@
 class JatController < ApplicationController
-  def create
-  end
+    def create
+    end
 
-  def save
-    time = Time.now
-  
-    @title = params[:title]
-    @layout = params[:layout]
-    @date = time.inspect
-    @content = params[:content]
-   
-    # format the title as lowercase with hyphens instead of spaces for the file name
-    @ftitle = @title.downcase.tr(" ", "-")
-   
-    # prepare the file name in the format "YYYY-MM-DD-hyphenated-title.md" with leading zeroes for single-digit months/days
-    @filename = time.year.to_s + "-" + time.month.to_s.rjust(2, "0") + "-" + time.day.to_s.rjust(2, "0") + "-" + @ftitle + ".md"
+    def save
+        time = Time.now
     
-    @data = @title
-    send_data(@data, :filename => @filename)
-  end
+        @title = params[:title]
+        @layout = params[:layout]
+        @date = time.inspect
+        @content = params[:content]
+     
+        # format the title as lowercase with hyphens instead of spaces for the file name
+        @ftitle = @title.downcase.tr(" ", "-")
+     
+        # prepare the file name in the format "YYYY-MM-DD-hyphenated-title.md" with leading zeroes for single-digit months/days
+        @filename = time.year.to_s + "-" + time.month.to_s.rjust(2, "0") + "-" + time.day.to_s.rjust(2, "0") + "-" + @ftitle + ".md"
+        
+        # create the file's content
+        @data = "---\n" \
+                + "layout: " + @layout + "\n" \
+                + "title: " + @title + "\n" \
+                + "date: " + @date + "\n" \
+                + "---\n\n" \
+                + @content
+        
+        # send the data to the user as a file download
+        send_data(@data, :filename => @filename)
+    end
 end
