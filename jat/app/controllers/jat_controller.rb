f1c37b192b8c491912cb6f72c6252ec30ffe075d
ian <ian@arch>
2014-06-22 00:57:13 -0400

    Base file generation + sending done

--- jat/app/controllers/jat_controller.rb
+++ jat/app/controllers/jat_controller.rb
@@ -7,5 +7,8 @@ class JatController < ApplicationController
     @layout = params[:layout]
     @date = Time.now.inspect
     @content = params[:content]
+    
+    @data = @title
+    send_data(@data, :filename => "test.md")
   end
 end
