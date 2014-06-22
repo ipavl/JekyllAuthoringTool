19bb54dba6206fcc26761ffe462164643dfe1a96
ian <ian@arch>
2014-06-22 01:13:58 -0400

    Hyphenated title

--- jat/app/controllers/jat_controller.rb
+++ jat/app/controllers/jat_controller.rb
@@ -10,10 +10,11 @@ class JatController < ApplicationController
     @date = time.inspect
     @content = params[:content]
    
-    @ftitle = ""
+    # format the title as lowercase with hyphens instead of spaces for the file name
+    @ftitle = @title.downcase.tr(" ", "-")
    
     # prepare the file name in the format "YYYY-MM-DD-hyphenated-title.md" with leading zeroes for single-digit months/days
-    @filename = time.year.to_s + "-" + time.month.to_s.rjust(2, "0") + "-" + time.day.to_s.rjust(2, "0") + "-" + ftitle + ".md"
+    @filename = time.year.to_s + "-" + time.month.to_s.rjust(2, "0") + "-" + time.day.to_s.rjust(2, "0") + "-" + @ftitle + ".md"
     
     @data = @title
     send_data(@data, :filename => @filename)
